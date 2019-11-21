362132a69c175ea98503e3c749be14d2d913d0f5
Joshua O'bannon <jtobannon@gmail.com>
2019-11-21 13:50:10 -0700

    complete story 4 functionality

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,6 +7,14 @@ class SheltersController < ApplicationController
     @shelter = Shelter.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    Shelter.create(shelter_params)
+    redirect_to '/shelters'
+  end
+
   def show
     @shelter = Shelter.find(params[:id])
   end
@@ -19,4 +27,9 @@ class SheltersController < ApplicationController
   #   task.save
   #   redirect_to "/shelters/#{shelter.id}"
   # end
+
+  private
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
+Shelter.destroy_all
+
 shelter_1= Shelter.create(name: "Purina Shelter")
 shelter_2= Shelter.create(name: "Blue Buffalo Shelter")
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)

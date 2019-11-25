46b91a2f971f5a8b00bc413931de8ed575af1862
Joshua O'bannon <jtobannon@gmail.com>
2019-11-25 14:14:42 -0700

    Work on story 8

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,14 +1,15 @@
 class PetsController < ApplicationController
   def index
     if params[:shelter_id]
-      @shelter = Shelter.find(params[:id])
-      @pets = @shelter.pets.all
+      @shelter = Shelter.find(params[:shelter_id])
+      @pets = @shelter.pets
     else
       @pets = Pet.all
     end
   end
 
   def show
+    binding.pry
     @pets = Pet.find(params[:id])
   end
 end
